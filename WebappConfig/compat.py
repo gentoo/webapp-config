20379939bf866babfd2dbec133d5cb59013ca451
Devan Franchini <twitch153@hotmail.com>
2013-09-22 20:40:09 -0400

    WebappConfig/compat.py: Adds compatibility check for file_md5().
    When the file_md5() function is called in python3.2+ a hash file is
    supposed to be created, and on a if an object is unicode, it needs to be
    encoded prior to hashing. In order to do this, a file called compat.py
    has a function in it called create_md5(); this function checks the
    hexidecimal python version and based on the version, either encodes the
    file before it hashes, or simply returns the md5 hash.
    
    Signed-off-by: Anthony G. Basile <blueness@gentoo.org>

---
+++ WebappConfig/compat.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python -O
+#
+# /usr/sbin/webapp-config
+#       Python script for managing the deployment of web-based
+#       applications
+#
+#       Originally written for the Gentoo Linux distribution
+#
+# Copyright (c) 1999-2007 Authors
+#       Released under v2 of the GNU GPL
+#
+# Author(s)     Stuart Herbert
+#               Renat Lumpau   <rl03@gentoo.org>
+#               Gunnar Wrobel  <wrobel@gentoo.org>
+#
+# ========================================================================
+
+import  hashlib, os, os.path, sys
+
+def create_md5(filename):
+    if hex(sys.hexversion) >= '0x3020000':
+        filename = open(filename).read()
+        encoded_file = filename.encode('utf8')
+        return str(hashlib.md5(encoded_file).hexdigest())
+    else:
+        return str(hashlib.md5(open(filename).read()).hexdigest())
+

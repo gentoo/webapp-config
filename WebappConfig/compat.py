c41a4b0cccc8425a9e00bd1c4cebaf50c058588f
Devan Franchini <twitch153@gentoo.org>
2013-10-28 22:17:00 -0400

    WebappConfig/compat.py: Alters warped compat file from cherry-picking.

--- WebappConfig/compat.py
+++ WebappConfig/compat.py
@@ -18,15 +18,5 @@
 import  hashlib
 
 def create_md5(filename):
-<<<<<<< HEAD
-    if hex(sys.hexversion) >= '0x3020000':
-        filename = open(filename).read()
-        encoded_file = filename.encode('utf8')
-        return str(hashlib.md5(encoded_file).hexdigest())
-    else:
-        return str(hashlib.md5(open(filename).read()).hexdigest())
-=======
     with open(filename, 'rb') as f:
         return str(hashlib.md5(f.read()).hexdigest())
->>>>>>> 04e9a55... WebappConfig/compat.py: Revamps create_md5() function.
-

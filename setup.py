5c7a8e632cfde1bceb5e2c071eadc5b2782f07a7
Devan Franchini <twitch153@gentoo.org>
2014-11-02 20:00:47 -0500

    setup.py: Adds Devan Franchini to list of authors

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from WebappConfig.version import WCVERSION
 setup(name          = 'webapp-config',
       version       = WCVERSION,
       description   = 'Python script for managing the deployment of web-based applications',
-      author        = 'Stuart Herbert, Renat Lumpau, Gunnar Wrobel',
+      author        = 'Stuart Herbert, Renat Lumpau, Gunnar Wrobel, Devan Franchini',
       author_email  = 'stuart@gentoo.org',
       url           = 'http://git.overlays.gentoo.org/gitweb/?p=proj/webapp-config.git;a=summary',
       packages      = ['WebappConfig'],
